27643bbabb4a0d8d6277e358f1c54ee197ff2546
git-zahidul-islam <freelancerzahidf101@gmail.com>
2024-05-14 01:24:14 +0600

    cookie added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken')
 const cookieParser = require('cookie-parser')
 const dotenv = require('dotenv')
 dotenv.config()
@@ -8,7 +9,12 @@ const app = express()
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors())
+app.use(cors({
+    origin: [
+        'http://localhost:5173',
+    ],
+    credentials: true
+}))
 app.use(express.json())
 app.use(cookieParser())
 
@@ -24,6 +30,29 @@ const client = new MongoClient(uri, {
     }
 });
 
+// middleware cookie
+const verifyToken = (req, res, next) => {
+    const token = req?.cookies?.token;
+    // console.log("the token in the middleware:",token);
+    if (!token) {
+        return res.status(401).send({ message: 'Unauthorized access' })
+    }
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ message: 'Unauthorized Access' })
+        }
+        req.user = decoded
+        next()
+    })
+}
+
+// cookie options
+const cookieOptions = {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production" ? true : false,
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "strict"
+}
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
@@ -31,6 +60,24 @@ async function run() {
         // database collection
         const assignmentsCollection = client.db('assignmentsDB').collection('assignment');
         const submitAssignmentCollection = client.db('assignmentsDB').collection('submitAssignment');
+
+        // auth related api [01] cookie start
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            // console.log("the jwt token: ", token);
+            res
+                .cookie('token', token, cookieOptions)
+                .send({ message: true })
+        })
+        app.post("/logout", async (req, res) => {
+            console.log("cookie logout", req.body);
+            res
+                .clearCookie('token', { ...cookieOptions, maxAge: 0 })
+                .send({ success: true })
+        })
+
+        // cookei end
 
         app.post('/assignments', async (req, res) => {
             // console.log("the data is:",req.body);
